d65030c7f566a24a7fff9bb9d7c4bde522002d89
Alexsey Balabushko <pro100alexsey97@mail.ru>
2018-09-18 08:14:25 +0200

    Added rounding up to an integer

--- lesson-5-closures/exercise-1.js
+++ lesson-5-closures/exercise-1.js
@@ -3,9 +3,9 @@
 var lattice = '';
 do {
   var rowS = prompt('Please, enter number of rows:', '8');
-  var row = parseInt(rowS, 0);
+  var row = Math.floor(parseInt(rowS, 0));
   var columnS = prompt('Please, enter number of columns:', '8');
-  var column = parseInt(columnS, 0);
+  var column = Math.floor(parseInt(columnS, 0));
 } while (isNaN(row) || isNaN(column));
 for (var i = 0; i < column; i++) {
   for (var j = 0; j < row; j++) {
