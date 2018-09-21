b0e45c8ca115837dae6fddd69f7be88a2f7ae934
Alexsey Balabushko <pro100alexsey97@mail.ru>
2018-09-21 17:02:43 +0200

    Change Ex1 and Ex2

--- lesson-3-basic-constructions/exercise.js
+++ lesson-3-basic-constructions/exercise.js
@@ -1,30 +1,23 @@
-while (1) {
+do {
   var fullName = prompt('Please, write your full name. Apart.', '');
-  if (/^[a-z]+ [a-z]+ [a-z]+$/i.test(fullName)) break;
-  alert('Invalid argument. Please, try again');
-}
-while (1) {
-  var ageInYears = prompt('Enter your age:', '');
-  if (parseInt(ageInYears, 0) > 0) break;
-  alert('Invalid argument. Please, try again');
-}
+} while (!fullName.match(/[a-z]+ [a-z]+ [a-z]/ig));
+
+do {
+  var ageInYearsS = prompt('Enter your age:', '');
+  var ageInYears = parseInt(ageInYearsS, 0);
+} while (isNaN(ageInYears) || (ageInYears <= 0) || (ageInYears) > 120);
+
 var gender = confirm('Are you a man?', '');
 var ageInDays = ageInYears * 365;
-var inFiveYears = +ageInYears + 5;
-var pension = 'no';
-if (gender === true && ageInYears >= 65) {
-  gender = 'male';
-  pension = 'yes';
-} else if (gender === true && ageInYears < 65) {
-  gender = 'male';
-  pension = 'no';
-} else if (gender === false && ageInYears >= 60) {
-  gender = 'female';
-  pension = 'yes';
-} else if (gender === false && ageInYears < 60) {
-  gender = 'female';
-  pension = 'no';
+var inFiveYears = ageInYears + 5;
+var pension = false;
+
+if (gender && (ageInYears >= 63)) {
+  pension = true;
+} else if (!gender && (ageInYears >= 58)) {
+  pension = true;
 }
+
+gender = (gender) ? 'male' : 'female';
+
 alert('Your full name: ' + fullName + '\n' + 'Your age in years: ' + ageInYears + '\n' + 'Your age in days: ' + ageInDays + '\n' + 'In 5 years you will be: ' + inFiveYears + '\n' + 'Your gender: ' + gender + '\n' + 'You are retired ' + pension);
-
-
--- lesson-3-basic-constructions/exercise2.js
+++ lesson-3-basic-constructions/exercise2.js
@@ -1,10 +1,13 @@
+'use strict';
+
 function considers() {
   var message = prompt('Enter your string:', '');
   var vowels = 'аеёиоуыэюя';
   var count = 0;
   for (var i = 0; i < message.length; i++) {
-    for (var j = 0; j < 10; j++) {
-      if (message[i] === vowels[j]) count++;
+    var C = message.toLowerCase().charAt(i);
+    if (vowels.indexOf(C) !== -1 ) {
+      count++;
     }
   }
   console.log('Number of vowels in string: ' + count);
