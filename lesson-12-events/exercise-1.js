7abe9ee196e42e23dcbbf6ceea82280fe191a632
Alexsey Balabushko <pro100alexsey97@mail.ru>
2018-12-01 11:21:21 +0200

    Modify exercise

--- lesson-12-events/exercise-1.js
+++ lesson-12-events/exercise-1.js
@@ -8,40 +8,49 @@ var DragShiftY = null;
 var imageIndex = 0;
 
 function imageAttributes() {
-  for (var i = 0; i < arrImage.length; i++) {
-    arrImage[i].style.width = '200px';
-    arrImage[i].style.height = '200px';
+  for (var i = arrImage.length - 1; i > -1; i--) {
+    var initialLeft = arrImage[i].offsetLeft;
+    var initialTop = arrImage[i].offsetTop;
+    arrImage[i].style.left = initialLeft + 'px';
+    arrImage[i].style.top = initialTop + 'px';
     arrImage[i].style.position = 'absolute';
-
-    arrImage[i].onmousedown = Drag_Start;
-    arrImage[i].onmouseup = Drag_Stop;
+    arrImage[i].addEventListener('mousedown', Drag_Start, false);
   }
 }
 
 function Drag_Start(EO) {
   EO = EO || window.event;
-  DragImage = EO.target;
   EO.preventDefault();
-  DragImage.onmousemove = Drag_Move;
+  DragImage = EO.target;
+
   DragShiftX = EO.pageX - DragImage.offsetLeft;
   DragShiftY = EO.pageY - DragImage.offsetTop;
-  DragImage.style.zIndex = imageIndex++;
   DragImage.style.cursor = 'grabbing';
 
+  window.addEventListener('mousemove', Drag_Move, false);
+  window.addEventListener('mouseup', Drag_Stop, false);
+  window.addEventListener('mouseout', Drag_Stop, false);
 }
 
 function Drag_Move(EO) {
   EO = EO || window.event;
+  EO.preventDefault();
   DragImage = EO.target;
+
+  DragImage.style.zIndex = imageIndex++;
   DragImage.style.left = EO.pageX - DragShiftX + 'px';
   DragImage.style.top = EO.pageY - DragShiftY + 'px';
 }
 
 function Drag_Stop(EO) {
   EO = EO || window.event;
+  EO.preventDefault();
   DragImage = EO.target;
-  DragImage.onmousemove = null;
+
   DragImage.style.cursor = 'default';
+  window.removeEventListener('mousemove', Drag_Move, false);
+  window.removeEventListener('mouseup', Drag_Stop, false);
+  window.removeEventListener('mouseout', Drag_Stop, false);
 }
 
 imageAttributes();
